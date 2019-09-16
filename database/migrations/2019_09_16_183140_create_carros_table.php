6fc25d97843cfc4168525ef348d1a59673ac74d8
fercell122 <ortyus@outlook.com>
2019-09-16 16:26:35 -0300

    second commit

---
+++ database/migrations/2019_09_16_183140_create_carros_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('marca');
+            $table->string('ano_fab');
+            $table->string('combustivel');
+            $table->string('renavam');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('carros');
+    }
+}
